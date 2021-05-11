d37d6673af4a11d782da5363f39211a07bbfad45
Joh-Ops <52340127+Joh-Ops@users.noreply.github.com>
2021-05-11 14:59:43 +1000

    hi

--- lib/Player.py
+++ lib/Player.py
@@ -5,4 +5,9 @@ class Player(pygame.sprite.Sprite):
 	def __init__(self):
 		super().__init__(self)
 
-		self.image = pygame.image.load('assets/stevencrowder')
+		self.image = pygame.image.load('assets/stevencrowder.jpg')
+		self.rect = self.image.get_rect()
+
+	def update(self):
+		pass
+
