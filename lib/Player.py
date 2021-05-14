2ffd58626f0d5becfa699a32dc609fd30944bd3f
Joh-Ops <52340127+Joh-Ops@users.noreply.github.com>
2021-05-14 16:16:00 +1000

    fixed movement

--- lib/Player.py
+++ lib/Player.py
@@ -2,21 +2,29 @@ import pygame
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, center_x=0, center_y=0):
         super().__init__()
-
         self.image = pygame.image.load('assets/stevencrowder.jpg').convert()
+
         self.rect = self.image.get_rect()
+        self.rect.center = (center_x, center_y)
 
     def update(self):
+        x = 0
+        y = 0
         keys_pressed = pygame.key.get_pressed()
-
-        # TODO: Make diagonal movement the same speed
         if keys_pressed[pygame.K_w]:
-            self.rect.move_ip(0, -10)
+            y -= 10
         if keys_pressed[pygame.K_s]:
-            self.rect.move_ip(0, 10)
+            y += 10
         if keys_pressed[pygame.K_d]:
-            self.rect.move_ip(10, 0)
+            x += 10
         if keys_pressed[pygame.K_a]:
-            self.rect.move_ip(-10, 0)
+            x -= 10
+
+        # If the player is moving diagonally, divide their speeds by sqrt(2) to keep the speed the same
+        if x and y:
+            x /= 2 ** 0.5
+            y /= 2 ** 0.5
+
+        self.rect.move_ip(x, y)
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pygame
+
+from config import *
 from lib.Player import Player
+
 
 pygame.init()
 
 # Set up window & FPS clock
 FPS = 60
 clock = pygame.time.Clock()
-window = pygame.display.set_mode((854, 480))
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("wumpus game")
 
 # changed_rects - list of Rect objects showing where the screen has updated
 changed_rects = []
-player = Player()
+player = Player(WINDOW_WIDTH/2, WINDOW_HEIGHT/2)
 
 background = pygame.image.load('assets/grokwallpaper.png').convert()
 changed_rects.append(window.blit(background, background.get_rect()))
@@ -19,29 +22,24 @@ changed_rects.append(window.blit(background, background.get_rect()))
 
 running = True
 while running:
-    # Process inputs
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             break
 
-    # Dirty rect animation https://www.pygame.org/docs/tut/newbieguide.html
-    # 1: Find the locations of all the sprites and add them to the list of places which need to be updated on the screen
-    changed_rects.append(player.rect)
 
-    # 2: Erase the old sprites with piece of the background
-    #window.blit(background, player.rect, player.rect)
-    window.blit(background, player.rect, player.rect)
+    # 1: Erase the old sprites with piece of the background and add their locations to the screen updating list
+    changed_rects.append(window.blit(background, player.rect, player.rect))
 
-    # 3: Update the locations of the sprites and blit them onto the window in the right order
+    # 2: Update the locations of the sprites and blit them onto the window in the right order
     player.update()
     window.blit(player.image, player.rect)
 
-    # 4: Add the new locations to the screen updating list
+    # 3: Add the new locations to the screen updating list
     changed_rects.append(player.rect)
 
-    # 5: Update the screen and clear the rects
-    pygame.display.update(changed_rects)
+    # 4: Update the screen and clear the rects
+    pygame.display.update()
     changed_rects = []
 
     clock.tick(FPS)
