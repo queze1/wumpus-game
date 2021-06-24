87ac83ed006227dcff265075cd337d7302c56a1b
haha-XD <toofastteemo@gmail.com>
2021-06-24 10:45:34 +1000

    menu

--- lib/Background.py
+++ lib/Background.py
@@ -6,3 +6,7 @@ from config import WINDOW_HEIGHT, WINDOW_WIDTH
 class Background(BaseSprite):
     def __init__(self):
         super().__init__(image_assets='assets/wallpaper.png', center=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+
+class MenuBackground(BaseSprite):
+    def __init__(self):
+        super().__init__(image_assets='assets/wallpaper.png', center=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
--- test.py
+++ test.py
@@ -1,47 +1,91 @@
-import random
-import pygame
-
+#!/usr/bin/python3.4
+# Setup Python ----------------------------------------------- #
+import pygame, sys
+ 
+# Setup pygame/window ---------------------------------------- #
+mainClock = pygame.time.Clock()
+from pygame.locals import *
 pygame.init()
-
-width, height = 400, 400
-window = pygame.display.set_mode((width, height))
-clock = pygame.time.Clock()
-
-x1, y1, r1, mx1, my1 = 200, 200, 50, (random.random() - 0.5) * 4, (random.random() - 0.5) * 4
-x2, y2, r2, mx2, my2 = 300, 200, 50, (random.random() - 0.5) * 4, (random.random() - 0.5) * 4
-
-def move(c, v, r, m):
-    c += v
-    if c < r: c, v = r, -v
-    if c > m-r: c, v = m-r, -v
-    return c, v
-
-hit_count = 0
-run = True
-while run:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-    x1, mx1 = move(x1, mx1, r1, width)
-    y1, my1 = move(y1, my1, r1, height)
-    x2, mx2 = move(x2, mx2, r2, width)
-    y2, my2 = move(y2, my2, r2, height)
-
-    v1 = pygame.math.Vector2(x1, y1)
-    v2 = pygame.math.Vector2(x2, y2)
-    if v1.distance_to(v2) < r1 + r2 - 2:
-        hit_count += 1
-        print("hit:", hit_count)
-
-        nv = v2 - v1
-        m1 = pygame.math.Vector2(mx1, my1).reflect(nv) * 1.1
-        m2 = pygame.math.Vector2(mx2, my2).reflect(nv) * 1.1
-        mx1, my1 = m1.x, m1.y
-        mx2, my2 = m2.x, m2.y
-
-    window.fill((127, 127, 127))
-    pygame.draw.circle(window, (255, 0, 0), (round(x1), round(y1)), r1, 4)
-    pygame.draw.circle(window, (0, 0, 255), (round(x2), round(y2)), r2, 4)
-    pygame.display.flip()
+pygame.display.set_caption('game base')
+screen = pygame.display.set_mode((500, 500),0,32)
+ 
+font = pygame.font.SysFont(None, 20)
+ 
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+ 
+click = False
+ 
+def main_menu():
+    while True:
+ 
+        screen.fill((0,0,0))
+        draw_text('main menu', font, (255, 255, 255), screen, 20, 20)
+ 
+        mx, my = pygame.mouse.get_pos()
+ 
+        button_1 = pygame.Rect(50, 100, 200, 50)
+        button_2 = pygame.Rect(50, 200, 200, 50)
+        if button_1.collidepoint((mx, my)):
+            if click:
+                game()
+        if button_2.collidepoint((mx, my)):
+            if click:
+                options()
+        pygame.draw.rect(screen, (255, 0, 0), button_1)
+        pygame.draw.rect(screen, (255, 0, 0), button_2)
+ 
+        click = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+ 
+        pygame.display.update()
+        mainClock.tick(60)
+ 
+def game():
+    running = True
+    while running:
+        screen.fill((0,0,0))
+        
+        draw_text('game', font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+        
+        pygame.display.update()
+        mainClock.tick(60)
+ 
+def options():
+    running = True
+    while running:
+        screen.fill((0,0,0))
+ 
+        draw_text('options', font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+        
+        pygame.display.update()
+        mainClock.tick(60)
+ 
+main_menu()
